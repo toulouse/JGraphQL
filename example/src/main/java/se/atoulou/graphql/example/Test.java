bc1e3c7bf9840d5c383157c99fb24115af4a876e
Andrew Toulouse <andrew@atoulou.se>
2015-08-09 14:53:15 -0700

    Implement schema grammar

---
+++ example/src/main/java/se/atoulou/graphql/example/Test.java
@@ -0,0 +1,24 @@
+package se.atoulou.graphql.example;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.Trees;
+
+import se.atoulou.parser.GraphQLSchemaLexer;
+import se.atoulou.parser.GraphQLSchemaParser;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+        InputStream in = Test.class.getResourceAsStream("/starWarsSchema.graphql");
+        ANTLRInputStream input = new ANTLRInputStream(in);
+        GraphQLSchemaLexer lexer = new GraphQLSchemaLexer(input);
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        GraphQLSchemaParser parser = new GraphQLSchemaParser(tokenStream);
+
+        System.out.println(Trees.toStringTree(parser.schemaDocument()));
+    }
+}
