c6f03d0f7ed3a75fc74cc2cb08058118d9df4bea
Andrew Toulouse <andrew@atoulou.se>
2015-08-13 21:03:10 -0700

    Add nullability annotations

---
+++ parser/src/main/java/se/atoulou/graphql/schema/NotNull.java
@@ -0,0 +1,11 @@
+package se.atoulou.graphql.schema;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE_USE, })
+@Retention(RetentionPolicy.CLASS)
+public @interface NotNull {
+}
